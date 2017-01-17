f07af08430442682b0a7c80e86e1b378a723565a
oguratakayuki <otn.ogura@gmail.com>
2017-01-17 21:28:37 +0900

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, :request_statusaa
   #http://stackoverflow.com/questions/38331496/rails-5-actioncontrollerinvalidauthenticitytoken-error
+
   protect_from_forgery with: :exception, prepend: true
 
   def request_statusaa
