7dc2ffe6e839ca5bb4dcd0d2050451215ce6e1bc
oguratakayuki <otn.ogura@gmail.com>
2017-03-08 21:02:41 +0900

    flow管理画面日本語化

--- app/models/flow_condition.rb
+++ app/models/flow_condition.rb
@@ -2,7 +2,7 @@ class FlowCondition < ApplicationRecord
   extend Enumerize
   belongs_to :flow_condition_group
   enumerize :related_model, in: %w!shop category sub_category price initial_cost time_required personal_number!, scope: true
-  enumerize :compare_type, in: %w!eq not_eq in not_in!, scope: true
+  enumerize :compare_type, in: %w!eq not_eq lt_eq gt_eq in not_in!, scope: true
   belongs_to :flow_condition_group
   has_many :flow_condition_options
   accepts_nested_attributes_for :flow_condition_options, allow_destroy: true
