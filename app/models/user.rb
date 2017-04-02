aaee3cac82d55bb73118e7dea2d5c2bd6c8ad691
oguratakayuki <otn.ogura@gmail.com>
2017-04-02 21:19:40 +0900

    ユーザー検索フォーム

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   include UserRoleEnumerations
   has_many :shop_users
   has_many :shops, through: :shop_users
+  has_many :brands, through: :shops
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
